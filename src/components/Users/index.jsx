114ea243df257c2aaa7340e80818cf8f6b493bd2
Christopher Peters <christopher.peters@itpeoplecorp.com>
2025-04-28 14:57:27 +0530

    added posts provider

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -4,12 +4,11 @@ import { UsersContext } from "../../context/UsersProvider";
 import { PostContext } from "../../context/PostProvider";
 
 const Users = () => {
-  const { name, users } = useContext(UsersContext);
+  const { users } = useContext(UsersContext);
   const { posts } = useContext(PostContext);
 
   return (
     <>
-      <p>{name}</p>
       <div className="grid grid-cols-3 gap-x-8 gap-y-4">
         {users.map((user) => (
           <Card user={user} post={posts?.[user?.id]} />
